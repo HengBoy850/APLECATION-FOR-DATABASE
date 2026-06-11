2d186b91c5d271aff5ddd790fa67a5fd7ab64735
unknown <hengboy850@gmail.com>
2026-06-11 14:58:36 +0700

    new

---
+++ F-Supermarket-Mangerment-System/src/components/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function footer() {
+  return (
+    <div>
+        <h1>
+            Yk pii Flowbite mk
+        </h1>
+      
+    </div>
+  )
+}
---
+++ F-Supermarket-Mangerment-System/src/components/navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function navbar() {
+  return (
+    <div>
+        <h1>Tver bb</h1>
+      
+    </div>
+  )
+}
